8bdba6ea177617cceac39ef4f1fdcc885d9c554d
Duke <duke@riseup.net>
2014-02-01 14:55:56 -0200

    added epl.org.br

--- ideiasliberdade.go
+++ ideiasliberdade.go
@@ -28,6 +28,7 @@ func main() {
 		"http://maovisivel.blogspot.com/feeds/posts/default",
 		"http://www.liberdade.cc/feed",
 		"http://ordemlivre.org/feed/blogs",
+		"http://epl.org.br/feed/",
 		"http://ordemlivre.org/feed/artigos",
 		"http://www.libertarianismo.org/index.php/category/artigos/feed/",
 	}
